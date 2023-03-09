6ef113c944354814403b2416bde110f657274c6c
JeonInguk <inguk12951@gmail.com>
2023-03-10 00:09:46 +0900

    Home화면 핫트렌드비디오 썸네일, 제목, ID 출력, 스타일컴포넌트 설치

--- src/conponent/pages/Home.jsx
+++ src/conponent/pages/Home.jsx
@@ -1,5 +1,34 @@
 import React from "react";
+import { useQuery } from "react-query";
 
 export default function Home() {
-  return <div>Home</div>;
+  const {
+    isLoading,
+    error,
+    data: hotVideo,
+  } = useQuery(["hotvideo"], async () => {
+    console.log("fetching...");
+    return fetch(`data/hot_trend_video.json`).then((res) => res.json());
+  });
+  console.log(hotVideo);
+  if (isLoading) return <p>Loading...</p>;
+
+  if (error) return <p>{error}</p>;
+  return (
+    <div className="video_item_container">
+      {hotVideo.items.map((video) => {
+        return (
+          <div className="video_item">
+            <img
+              className="hotvideoitem"
+              src={video.snippet.thumbnails.medium.url}
+              alt="썸네일"
+            ></img>
+            <h5>{video.snippet.title}</h5>
+            <h6>{video.snippet.channelId}</h6>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
